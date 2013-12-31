1f55ac60cdfe0ae6fc81196c6adc2015e3fbd45e
Jason Gill <jason.gill@gmomail.org>
2013-12-31 11:49:29 -0600

    reformatted file

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,73 +10,73 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-      pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package.json'),
 
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        'test/*_test.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'test/*_test.js'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-    mochaTest: {
-      all: {
-        options: {
-          reporter: 'spec'
-        },
-        src: ['test/*_test.js']
-      }
-    }
+		mochaTest: {
+			all: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/*_test.js']
+			}
+		}
 
-  });
+	});
 
-    grunt.registerTask('init', function() {
-        grunt.log.writeln('Running init...');
-        return true;
-    });
-    grunt.registerTask('phpunit', function() {
-        grunt.log.writeln('Running phpunit tests...');
-        return true;
-    });
-    grunt.registerTask('mocha', function() {
-        grunt.log.writeln('Running mocha tests...');
-        return true;
-    });
-    grunt.registerTask('cleanUp', function() {
-        grunt.log.writeln('Running clean up...');
-        return true;
-    });
+	grunt.registerTask('init', function() {
+		grunt.log.writeln('Running init...');
+		return true;
+	});
+	grunt.registerTask('phpunit', function() {
+		grunt.log.writeln('Running phpunit tests...');
+		return true;
+	});
+	grunt.registerTask('mocha', function() {
+		grunt.log.writeln('Running mocha tests...');
+		return true;
+	});
+	grunt.registerTask('cleanUp', function() {
+		grunt.log.writeln('Running clean up...');
+		return true;
+	});
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-mocha-test');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-mocha-test');
 
-  // Tasks
-  grunt.registerTask('build', ['clean','jshint']);
-  grunt.registerTask('default', ['build', 'mochaTest']);
+	// Tasks
+	grunt.registerTask('build', ['clean', 'jshint']);
+	grunt.registerTask('default', ['build', 'mochaTest']);
 
 
-    grunt.registerTask('runTests', ['phpunit','mocha']);
+	grunt.registerTask('runTests', ['phpunit', 'mocha']);
 
-    grunt.registerTask('preDeployment', ['init','runTests']);
-    grunt.registerTask('postDeployment', ['cleanUp']);
+	grunt.registerTask('preDeployment', ['init', 'runTests']);
+	grunt.registerTask('postDeployment', ['cleanUp']);
 
-    //grunt.registerTask('deployToProd', ['deployCodeTo:prod:sandbox']);
+	//grunt.registerTask('deployToProd', ['deployCodeTo:prod:sandbox']);
 
 };
